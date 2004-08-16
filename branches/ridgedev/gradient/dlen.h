c586da0a3c2c3823ae5883a264b055f185f9c154
michaelp <michaelp@ee954a25-e447-0410-aa69-e04174172ac8>
2004-08-16 21:49:55 +0000

    Initial ridgerunner import

---
+++ branches/ridgedev/gradient/dlen.h
@@ -0,0 +1,19 @@
+/*
+ *  dlen.h
+ *  ridgerunner3
+ *
+ *  Created by Michael Piatek on Sun Jan 18 2004.
+ *  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+ *
+ */
+#ifndef _H_dlen
+#define _H_dlen
+
+#include "octrope.h"
+#include "stepper.h"
+
+// note that we return dlen as a concatenated list of dlen for 
+// each component in ascending order
+void dlenForce( octrope_vector* ioDL, octrope_link* inLink, search_state* inState );
+
+#endif // _H_dlen
---
+++ branches/ridgedev/settings.c
@@ -0,0 +1,13 @@
+/*
+ *  settings.c
+ *  ridgerunner
+ *
+ *  Created by Michael Piatek on Thu Jun 03 2004.
+ *  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "settings.h"
+
+RSettings   gRidgeSettings;
+
