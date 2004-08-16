849bd946ea70caadd18fa7158a4facba985bec3b
michaelp <michaelp@ee954a25-e447-0410-aa69-e04174172ac8>
2004-08-16 22:35:34 +0000

    updating project structure to work with cvs

---
+++ trunk/mactrope/truefalse.h
@@ -0,0 +1,20 @@
+/*
+ * @COPYRIGHT@
+ *
+ * Define TRUE and FALSE
+ *
+ *  $Id: truefalse.h,v 1.1 2004-08-16 22:35:30 michaelp Exp $
+ *
+ */
+
+#ifndef __TRUEFALSE_H
+#define __TRUEFALSE_H
+
+#ifndef FALSE
+#define FALSE (1 == 0)
+#endif /* FALSE */
+#ifndef TRUE
+#define TRUE (1 == 1)
+#endif /* TRUE */
+
+#endif
