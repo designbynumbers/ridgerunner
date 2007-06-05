90c24626744e87cfcb5e5a2690906ff42e0ea92d
Jason Cantarella <jason.cantarella@gmail.com>
2007-06-05 15:33:01 +0000

    Added source files

---
+++ src/errors.h
@@ -0,0 +1,72 @@
+/*
+ *  errors.h
+ *  ridgerunner3
+ *
+ *  Created by Michael Piatek on Fri Jan 16 2004.
+ *  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+ *
+ */
+#ifndef _H_errors
+#define _H_errors
+
+#include "octrope.h"
+
+#ifndef true
+#define true (1==1)
+#endif
+#ifndef false
+#define false (1==0)
+#endif
+
+enum 
+{
+    kNoErr              = 0,
+    kNULLPointer        = 100,
+    kFileIOErr          = 200,
+    kUnsupportFileType  = 201,
+    kGenericNumerics    = 300
+};
+
+void DebugThrow( int inErr, const char* inFile, long inLine );
+void DebugWarning( int inErr, const char* inFile, long inLine );
+
+void error_write( octrope_link* inLink );
+
+#define fatal_(err) DebugThrow( (err), __FILE__, __LINE__ )
+#define warning_(err) DebugWarning( (err), __FILE__, __LINE__ )
+
+// we need to wrap these extended defines in do-while statements so the 
+// semicolons make sense and code blocks are self-contained
+#define fatalifnull_(ptr)                   \
+            do {                            \
+                if( (ptr) == NULL )         \
+                    fatal_(kNULLPointer);   \
+            } while( false ) 
+            
+#define warningifnull_(ptr)                 \
+            do {                            \
+                if( (ptr) == NULL )         \
+                    warning_(kNULLPointer); \
+            } while( false ) 
+
+#define fataliferr_(err)                    \
+            do {                            \
+                int __theErr = err;         \
+                if( __theErr != kNoErr )    \
+                {                           \
+                    fatal_(__theErr);       \
+                }                           \
+            } while( false )  
+
+#define warningiferr_(err)                  \
+            do {                            \
+                int __theErr = err;         \
+                if( __theErr != kNoErr )     \
+                {                           \
+                    warning_(__theErr);     \
+                }                           \
+            } while( false )  
+
+
+
+#endif // _H_errors
---
+++ src/settings.c
@@ -0,0 +1,34 @@
+/*
+ *  settings.c
+ *  ridgerunner
+ *
+ *  Created by Michael Piatek on Thu Jun 03 2004.
+ *  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "settings.h"
+
+#include <sys/types.h>
+#include <unistd.h>
+
+RSettings   gRidgeSettings;
+
+extern int gAvoidTmpConflicts;
+
+void
+preptmpname( char* outName, const char* inName, search_state* inState )
+{
+	if( gAvoidTmpConflicts != 0 )
+	{
+		if( inState != NULL )
+			sprintf( outName, "/tmp/%d.%s-%s", getpid(), inState->fname, inName );
+		else
+			sprintf( outName, "/tmp/%d-%s", getpid(), inName );
+	}
+	else
+	{
+		sprintf(outName, "/tmp/%s", inName);
+	}
+}
+
