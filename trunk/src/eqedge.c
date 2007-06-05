92436c06c914d33aaab0129371fcc7051f1289a4
cantarel <cantarel@ee954a25-e447-0410-aa69-e04174172ac8>
2007-06-05 19:15:45 +0000

    Removed eqedge stuff containing old equilateralization code.

--- trunk/src/eqedge.c
+++ trunk/src/eqedge.c
@@ -12,14 +12,6 @@
 
 #include"eqedge.h"
 
-#ifdef __APPLE__
-#include <vecLib/vBLAS.h>
-#include <vecLib/clapack.h>
-#else
-#include "gsl_cblas.h"
-#endif // __APPLE__
-
-
 void    clapack_matrix_write(double val, double *A, int LDA, int i, int j)
 
      /* Function writes val to position (i,j) in the LDAxN lapack matrix A. */
@@ -29,61 +21,60 @@ void    clapack_matrix_write(double val, double *A, int LDA, int i, int j)
 }
 
 double  clapack_matrix_read(double *A, int LDA, int i, int j)
-
+     
      /* Function returns a value from position (i,j) in LDA x N lapack matrix A. */
-
+     
 {
   return A[j*LDA + i];
 }
 
 void    clapack_matrix_print(double *A, int M, int N) 
-
-     /* Function prints the M x N lapack matrix A. */
-
+/* Function prints the M x N lapack matrix A. */
+     
 {
   int i, j;
-
-  for(i=0;i<M;i++) {
   
+  for(i=0;i<M;i++) {
+    
     for(j=0;j<N;j++) {
-
+      
       printf("%3.2g ",clapack_matrix_read(A,M,i,j));
-
+      
     }
-
+    
     printf("\n");
 
   }
-
+  
   printf("\n");
-
+  
 }
 
-double *tangential_rigidity_matrix(octrope_link *L, int comp, int *LDA, int *N)
+double *tangential_rigidity_matrix(plCurve *L, int comp, int *LDA, int *N)
 
      /* Function returns a pointer to an LDAxN lapack-style matrix which encodes
 	the change in edge lengths of component <comp> under the tangential variation 
 	of its vertices. */
-
+     
 {
 
   int i;
   double *A;
-  octrope_vector LeftTan,RightTan,EdgeDir;
+  plc_vector LeftTan,RightTan,EdgeDir;
 
   /* 
      
-     We first allocate the matrix A. The dimensions of A are:
-
-        <# of edges of <comp>> x <#of vertices of <comp>>  
-
-     The number of edges is equal to the number of vertices if
-     the polyline is closed (acylic == FALSE) and is one less than
-     the number of vertices otherwise.
-
+  We first allocate the matrix A. The dimensions of A are:
+  
+  <# of edges of <comp>> x <#of vertices of <comp>>  
+  
+  The number of edges is equal to the number of vertices if
+  the polyline is closed (acylic == FALSE) and is one less than
+  the number of vertices otherwise.
+  
   */
 
-  *LDA = octrope_pline_edges(&L->cp[comp]);
+  *LDA = plc_strand_edges(&L->cp[comp]);
   *N   = L->cp[comp].nv;
 
   A = calloc( (*LDA) * (*N), sizeof(double) );
@@ -96,12 +87,12 @@ double *tangential_rigidity_matrix(octrope_link *L, int comp, int *LDA, int *N)
     /* This is nonzero only when j is an endpoint of the edge, so we first collect */
     /* the tangent vectors at the ends of the edge. */
 
-    LeftTan = octrope_link_tangent_vector(L,comp,i);
-    RightTan = octrope_link_tangent_vector(L,comp,i+1);
-    EdgeDir = octrope_link_edge_dir(L,comp,i);
+    LeftTan = plCurve_tangent_vector(L,comp,i);
+    RightTan = plCurve_tangent_vector(L,comp,i+1);
+    EdgeDir = plCurve_edge_dir(L,comp,i);
 
-    clapack_matrix_write( -octrope_dot(EdgeDir,LeftTan) ,A,*LDA,i,i  );
-    clapack_matrix_write( octrope_dot(EdgeDir,RightTan),A,*LDA,i,i+1);
+    clapack_matrix_write( -plc_M_dot(EdgeDir,LeftTan) ,A,*LDA,i,i  );
+    clapack_matrix_write( plc_M_dot(EdgeDir,RightTan),A,*LDA,i,i+1);
 
   }
 
@@ -111,12 +102,12 @@ double *tangential_rigidity_matrix(octrope_link *L, int comp, int *LDA, int *N)
   
   if (!L->cp[comp].acyclic) {
 
-    LeftTan = octrope_link_tangent_vector(L,comp,i);
-    RightTan = octrope_link_tangent_vector(L,comp,0);
-    EdgeDir = octrope_link_edge_dir(L,comp,i);
+    LeftTan = plCurve_tangent_vector(L,comp,i);
+    RightTan = plCurve_tangent_vector(L,comp,0);
+    EdgeDir = plCurve_edge_dir(L,comp,i);
 
-    clapack_matrix_write( -octrope_dot(EdgeDir,LeftTan) ,A,*LDA,i,i);
-    clapack_matrix_write( octrope_dot(EdgeDir,RightTan),A,*LDA,i,0);
+    clapack_matrix_write( -plc_M_dot(EdgeDir,LeftTan) ,A,*LDA,i,i);
+    clapack_matrix_write( plc_M_dot(EdgeDir,RightTan),A,*LDA,i,0);
 
   } 
 
@@ -131,10 +122,10 @@ double *tangential_rigidity_matrix(octrope_link *L, int comp, int *LDA, int *N)
   
 }
 
-double *edgelength_error(octrope_link *L,int comp, search_state* inState)
+double *edgelength_error(plCurve *L,int comp, search_state* inState)
 
      /* Procedure computes the difference between each edge length and
-	the "target" value of octrope_pline_length/octrope_pline_edges,
+	the "target" value of plc_strand_length/plc_strand_edges,
 	and returns it as a vector.  The argument LDB returns the number
         of rows in this vector. */
 
@@ -142,9 +133,9 @@ double *edgelength_error(octrope_link *L,int comp, search_state* inState)
   double *Err;
   int    i,edges;
 
-  octrope_vector V;
+  plc_vector V;
 
-  edges = octrope_pline_edges(&(L->cp[comp]));
+  edges = plc_strand_edges(&(L->cp[comp]));
   Err = calloc(edges,sizeof(double));
   
   /* We now compute the raw edge lengths and total length. */
@@ -152,8 +143,8 @@ double *edgelength_error(octrope_link *L,int comp, search_state* inState)
   for(i=0;i<edges;i++) {
 
     V = L->cp[comp].vt[i];
-    octrope_vsub(V,L->cp[comp].vt[i+1]);      
-    Err[i] = octrope_norm(V);
+    plc_M_sub_vect(V,L->cp[comp].vt[i+1]);      
+    Err[i] = plc_M_norm(V);
     
   }
 
@@ -162,7 +153,7 @@ double *edgelength_error(octrope_link *L,int comp, search_state* inState)
   for(i=0;i<edges;i++) {
 
    // Err[i] -= target_length;
-	Err[i] -= inState->conserveLength[i+inState->compOffsets[comp]];
+    Err[i] -= inState->conserveLength[i+inState->compOffsets[comp]];
 
   }
 
@@ -172,7 +163,7 @@ double *edgelength_error(octrope_link *L,int comp, search_state* inState)
 
 }
 
-void lapack_eqedge(octrope_link *L, int nsteps, search_state* inState)
+void lapack_eqedge(plCurve *L, int nsteps, search_state* inState)
 
      /* Procedure equilateralizes L using <nsteps> Newton steps.
 	This procedure should converge exponentially in <nsteps>, with good stability
@@ -210,168 +201,171 @@ void lapack_eqedge(octrope_link *L, int nsteps, search_state* inState)
   int    step, comp;
   double target_length;
 
-  octrope_vector stepV;
+  plc_vector stepV;
 
   int    i;
   
   // we pretend
 
   if (VERBOSITY > 2) {		/* Debugging code records function entry. */
-
+    
     fprintf(stderr,"lapack_eqedge called. \n\n");
     fprintf(stderr,"  %d component link.\n",L->nc);
 
   }
-
+  
   if (GRAPHICS) {
-
+    
     refresh_display(L);
     i = 1;
   }
 
   for(comp = 0;comp < L->nc; comp++) {
 
-    target_length = octrope_pline_length(&L->cp[comp])/(double)(octrope_pline_edges(&L->cp[comp]));
-
+    target_length = plc_strand_length(&L->cp[comp])/
+      (double)(plc_strand_edges(&L->cp[comp]));
+    
     for(step = 0; step < nsteps; step++) {
+      
+      if (VERBOSITY > 3) {	/* Track maximum Errors before step. */
 
-		  if (VERBOSITY > 3) {	/* Track maximum Errors before step. */
+	fprintf(stderr,"    %d vertex component (# %d)\n",L->cp[comp].nv,comp);
+	fprintf(stderr,"    maxErr before step %d = %g.\n",
+		step,maxError(L,comp,inState));
+	fprintf(stderr,"    max/min before step %d = %g.\n\n",
+		step,plCurve_long_edge(L)/plCurve_short_edge(L));
+	
+      }
+      
+      /* 1) Find the rigidity matrix. */
+      
+      A = (__CLPK_doublereal *)
+	(tangential_rigidity_matrix(L,comp,(int *)(&LDA),(int *)(&N)));
+      
+      /* 2) Find the edge length errors. */
 
-			fprintf(stderr,"    %d vertex component (# %d)\n",L->cp[comp].nv,comp);
-			fprintf(stderr,"    maxErr before step %d = %g.\n",step,maxError(L,comp,inState));
-			fprintf(stderr,"    max/min before step %d = %g.\n\n",step,octrope_link_long_edge(L)/octrope_link_short_edge(L));
+      Err = (__CLPK_doublereal *)(edgelength_error(L,comp,inState));
 
-		  }
+      /* 3) Solve the least-squares problem */
+      
+      LWORK = 20*LDA;
+      WORK = malloc(LWORK*sizeof(double));
+      M = LDA; NRHS = 1; 
+      RCOND = -1.0; /* Use machine precision */
+      S = malloc(M*sizeof(double));
+      
+      /* We now set up the RHS B. Paradoxically, though B is a vector
+	 of length M (# edges), we must allocate a vector of length
+	 _N_ (3 * # verts) because this space will be overwritten by
+	 the least-squares _solution_ to the problem.
+	 
+	 We similarly set LDB = N even though there are only M nonzero
+	 elements when we pass B to dgelss. */
+      
+      B = calloc(N,sizeof(double));
+      
+      for(i=0;i<M;i++) {
+	
+	B[i] = Err[i];
 
-		  /* 1) Find the rigidity matrix. */
+      } 
 
-		  A = (__CLPK_doublereal *)(tangential_rigidity_matrix(L,comp,(int *)(&LDA),(int *)(&N)));
+      LDB = N;
+      
+      /* We now call dgelss_, the underdetermined least-squares solver. */
+      
+      double* newA = (double*)calloc(M*N,sizeof(double));
+      int rItr, cItr;
+      
+      for( rItr=0; rItr<M; rItr++ )
+	{
+	  for( cItr=0; cItr<N; cItr++ )
+	    newA[rItr*N + cItr] = clapack_matrix_read(A, LDA, rItr, cItr);
+	}
+      
+      reloadDump(A, M, N, B, Err);
+      dgelss_(&M, &N, &NRHS, A, &LDA, B, &LDB, S, &RCOND, &RANK, WORK, &LWORK, &INFO);
+      reloadDump(A, M, N, B, Err);
 
-		  /* 2) Find the edge length errors. */
-
-		  Err = (__CLPK_doublereal *)(edgelength_error(L,comp,inState));
-
-		  /* 3) Solve the least-squares problem */
-
-		  LWORK = 20*LDA;
-		  WORK = malloc(LWORK*sizeof(double));
-		  M = LDA; NRHS = 1; 
-		  RCOND = -1.0; /* Use machine precision */
-		  S = malloc(M*sizeof(double));
+      free(newA);
 		  
-		  /* We now set up the RHS B. Paradoxically, though B is a vector of length M (# edges),
-		 we must allocate a vector of length _N_ (3 * # verts) because this space will be 
-		 overwritten by the least-squares _solution_ to the problem.
+      /* We now check INFO to make sure the method converged. */
 
-		 We similarly set LDB = N even though there are only M nonzero elements when we 
-		 pass B to dgelss. */
+      if (INFO != 0) {
 
-		  B = calloc(N,sizeof(double));
+	if (INFO < 0) {
+	  
+	  fprintf(stderr,"lapack_eqedge: cDGELSS reports " \
+		  "illegal %d th argument.\n",(int)(-INFO));
+	  exit(1);
 
-		  for(i=0;i<M;i++) {
-
-				B[i] = Err[i];
-
-		  } 
-
-		  LDB = N;
-
-		  /* We now call dgelss_, the underdetermined least-squares solver. */
-			
-						double* newA = (double*)calloc(M*N,sizeof(double));
-			int rItr, cItr;
-			
-			for( rItr=0; rItr<M; rItr++ )
-			{
-				for( cItr=0; cItr<N; cItr++ )
-					newA[rItr*N + cItr] = clapack_matrix_read(A, LDA, rItr, cItr);
-			}
-
-			reloadDump(A, M, N, B, Err);
-			
-			
-		  dgelss_(&M, &N, &NRHS, A, &LDA, B, &LDB, S, &RCOND, &RANK, WORK, &LWORK, &INFO);
-		  
-						reloadDump(A, M, N, B, Err);
-
-		  
-			
-			free(newA);
-		  
-
-		  /* We now check INFO to make sure the method converged. */
-
-		  if (INFO != 0) {
-
-			if (INFO < 0) {
-
-			  fprintf(stderr,"lapack_eqedge: cDGELSS reports illegal %d th argument.\n",(int)(-INFO));
-			  exit(1);
-
-			} else {
-
-			  fprintf(stderr,"lapack_eqedge: cDGELSS couldn't find SVD for rigidity matrix.\n");
-			  exit(1);
-
-			}
-
-		  }
-
-		  /* 4) Take a Newton step. */
-
-		  for(i=0;i<L->cp[comp].nv;i++) {
-
-			stepV = octrope_link_tangent_vector(L,comp,i);
-			octrope_vsmult(B[i],stepV);
-			octrope_vsub((L->cp[comp].vt[i]),stepV);
-
-		  }
-
-      octrope_link_fix_wrap(L);
-
+	} else {
+	  
+	  fprintf(stderr,"lapack_eqedge: cDGELSS couldn't " \
+		  "find SVD for rigidity matrix.\n");
+	  exit(1);
+	  
+	}
+	
+      }
+      
+      /* 4) Take a Newton step. */
+      
+      for(i=0;i<L->cp[comp].nv;i++) {
+	
+	stepV = plCurve_tangent_vector(L,comp,i);
+	plc_M_scale_vect(B[i],stepV);
+	plc_M_sub_vect((L->cp[comp].vt[i]),stepV);
+	
+      }
+      
+      plc_fix_wrap(L);
+      
       if (VERBOSITY > 3) { /* Report on the step we just took. */
-
-		double maxStep;
-
-		for(maxStep = 0,i=0;i<LDB;i++) {
-
-		  maxStep = (maxStep > fabs((double)(B[i])) ? maxStep : fabs((double)(B[i])));
-
-		}
-				 
-		fprintf(stderr,"   Maximum coordinate movement %g.\n",maxStep);
-		fprintf(stderr,"   Maximum error after step    %g.\n",maxError(L,comp,inState));
-		fprintf(stderr,"   max/min after step          %g.\n\n",octrope_link_long_edge(L)/octrope_link_short_edge(L));
-
+	
+	double maxStep;
+	
+	for(maxStep = 0,i=0;i<LDB;i++) {
+	  
+	  maxStep = (maxStep > fabs((double)(B[i])) ? maxStep : fabs((double)(B[i])));
+	  
+	}
+	
+	fprintf(stderr,"   Maximum coordinate movement %g.\n",maxStep);
+	fprintf(stderr,"   Maximum error after step    %g.\n",
+		maxError(L,comp,inState));
+	fprintf(stderr,"   max/min after step          %g.\n\n",
+		plCurve_long_edge(L)/plCurve_short_edge(L));
+	
       }
 
       if (GRAPHICS) {
-
-		refresh_display(L);
-
+	
+	refresh_display(L);
+	
       }
-
+      
       /* 5) Free the memory that we allocated. */
-
+      
       free(WORK); free(S); free(A); free(Err); free(B);
-
+      
     }
-
+    
   } /* End of main stepping loop. */
 
 }
 
-double maxError(octrope_link *L, int comp, search_state* inState) 
-
+double maxError(plCurve *L, int comp, search_state* inState) 
+     
      /* Computes the maximum error in edge lengths. */
-
+     
 {
   int i;
   double maxE = 0, *Err;
   
   Err = edgelength_error(L,comp,inState);
   
-  for(i=0;i<octrope_pline_edges(&L->cp[comp]);i++) {
+  for(i=0;i<plc_strand_edges(&L->cp[comp]);i++) {
 
     maxE = (fabs(Err[i]) > maxE) ? fabs(Err[i]) : maxE;
 
