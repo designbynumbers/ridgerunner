a17f900510e47280a2ea1420f25dad9d1b29d187
Jason Cantarella <jason.cantarella@gmail.com>
2010-07-19 14:55:48 +0000

    added residual c program

---
+++ src/residual.c
@@ -0,0 +1,138 @@
+/* 
+ * residual.c : This short program computes the residual of a given vect file.
+                We link with a number of the ridgerunner object files to do this.
+ *
+ */
+
+#include "portability.h"
+#include "octrope.h"
+#include "ridgerunner.h"
+#include <argtable2.h>
+
+int main(int argc,char *argv[]) {
+
+  FILE *infile_fptr = {NULL};
+  double lambda = 1.0,tuberad = 0.5;
+  plCurve *L;
+
+  int nerrors;
+    
+  struct arg_file *infile = arg_filen(NULL,NULL,"<VECT file>", 1, 10000, "input VECT file(s)");
+  struct arg_dbl  *tube_radius = arg_dbl0("r","radius","<x>","radius of tube");
+  struct arg_dbl  *arg_lambda = arg_dbl0("l","lambda","<x>","minimum diameter of curvature");
+  struct arg_lit  *help        = arg_lit0("h","help","display help message");
+  struct arg_end  *end = arg_end(20);
+
+  void *argtable[] = {infile,tube_radius,arg_lambda,help,end}; 
+  
+  struct arg_end  *helpend = arg_end(20);
+
+  void *helptable[] = {help, helpend};
+  int filenum;
+  char revision[20] = "$Revision: 1.20 $";
+  char *dollar;
+
+  dollar = strchr(&revision[1],'$');
+  dollar[0] = '\0';
+  printf("residual v%s, ridgerunner v%s\n",&revision[11],PACKAGE_VERSION);
+ /* The PACKAGE_VERSION preprocessor symbol is defined in "config.h" by autoconf. */
+
+  printf("  computes the ridgerunner residual of a polygonal knot.\n");
+  
+  /* We start by parsing the command-line arguments with argtable. */
+  
+  if (arg_nullcheck(argtable) != 0 || arg_nullcheck(helptable) != 0)
+    printf("error: insufficient memory\n");
+
+  nerrors = arg_parse(argc,argv,argtable);
+  
+  if (nerrors > 0) { /* The standard syntax didn't match-- try the help table */
+
+    nerrors = arg_parse(argc,argv,helptable);
+
+    if (nerrors > 0) {  /* The help table didn't match either-- the first set of */
+                        /* errors was probably more helpful, so we display it. */
+
+      arg_print_errors(stdout,helpend,"helptable");
+      arg_print_errors(stdout,end,"struts");
+      exit(1);
+
+    } else {  /* The help table matched, which means we asked for help or gave nothing */
+  
+      printf("residual computes the fraction of the gradient of length for a polygonal knot \n"
+	     "which is unresolved by strut and kink compression forces. This provides a measure\n"
+	     "of how close the configuration is to being (polygonal) ropelength-critical.\n"
+	     "usage: \n\n");
+      arg_print_glossary(stdout, argtable," %-25s %s\n");
+      exit(0);
+
+    }
+    
+  }
+  
+  /* Convert command-line arguments into proper global variables */
+  
+  if (arg_lambda->count > 0) {
+
+    lambda = arg_lambda->dval[0];
+
+  }
+
+  if (tube_radius->count > 0) {
+
+    tuberad = tube_radius->dval[0];
+
+  }
+  
+  search_state inState;
+  inState.dumpAxb = 0;
+  inState.snapinterval = 1000;
+  inState.steps = 0;
+
+  const char *FILENAME;
+
+  for (filenum = 0;filenum < infile->count;filenum++) {
+    
+    FILENAME = infile->filename[filenum];
+    
+    infile_fptr = fopen(infile->filename[filenum],"r");
+    
+    if (infile_fptr == NULL) {
+      
+      fprintf(stderr,"residual: Couldn't open file %s.\n",infile->filename[filenum]);
+      continue;
+      
+    }
+    
+    octrope_error_num = 0;
+    L = plc_read(infile_fptr,&octrope_error_num,octrope_error_str,80);
+    
+    /* We now demonstrate the octrope library's error handling protocol: */
+    
+    if (octrope_error_num > 0) {   /* This is the signal for an error. */
+      
+      fprintf(stderr,"residual: link reading error\n%s\n",octrope_error_str);
+      continue;
+      
+    }
+    
+    fclose(infile_fptr);
+  
+    /* The actual computation is performed as a side effect of stepDirection. */
+
+    stepDirection(L,tuberad,0,lambda,&inState);
+
+    /* We now have the data inside inState. */
+
+    printf("%s rop: %g thi: %g struts: %d mrstruts: %d residual: %g \n",
+	   infile->basename[filenum],inState.ropelength,inState.thickness,
+	   inState.lastStepStrutCount,inState.lastStepMinradStrutCount,inState.residual);
+
+    plc_free(L);
+
+  }
+
+  exit(0);
+  
+}
+	   
